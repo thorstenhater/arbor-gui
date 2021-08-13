84673e782ff714cb31c3a7ed54a9f33b2fd2c55c
Brent Huisman <brent@huisman.pl>
2021-08-13 12:48:09 +0200

    misschien deze dan

---
+++ scripts/appimage.sh
@@ -0,0 +1,12 @@
+cd ~/tmp
+curl -L -O "https://github.com/probonopd/linuxdeployqt/releases/download/7/linuxdeployqt-7-x86_64.AppImage"
+chmod a+x linuxdeployqt*.AppImage
+unset QTDIR
+unset QT_PLUGIN_PATH
+unset LD_LIBRARY_PATH
+./linuxdeployqt*.AppImage --appimage-extract-and-run ~/AppDir/share/applications/*.desktop -bundle-non-qt-libs -verbose=2
+
+./linuxdeployqt*.AppImage --appimage-extract-and-run ~/AppDir/share/applications/*.desktop -appimage -verbose=2
+
+cp *.AppImage* /${GITHUB_WORKSPACE}/arbor-gui.AppImage
+ls -l /${GITHUB_WORKSPACE}/arbor-gui.AppImage
--- scripts/set-linux-desktop-file.sh
+++ scripts/set-linux-desktop-file.sh
@@ -3,28 +3,9 @@ respath=$1
 iconpng=$2
 iconsvg=$3
 desktop=$4
-# mkdir -p $respath
-# cp $iconpng $respath
-userid=$(id -u)
-if [ $userid -eq 0 ]
-  then
-  mkdir -p /usr/local/share/applications/
-  cp $desktop /usr/local/share/applications/
-  mkdir -p /usr/share/icons/hicolor/scalable/apps/
-  mkdir -p /usr/share/icons/hicolor/256x256/apps/
-  cp $iconpng /usr/share/icons/hicolor/256x256/apps/
-  cp $iconsvg /usr/share/icons/hicolor/scalable/apps/
-#   cp $iconpng /usr/share/icons/
-#   cp $iconsvg /usr/share/icons/
-  exit
-else
-  mkdir -p ~/.local/share/applications/
-  cp $desktop ~/.local/share/applications/
-  mkdir -p ~/.local/share/icons/hicolor/scalable/apps/
-  mkdir -p ~/.local/share/icons/hicolor/256x256/apps/
-  cp $iconpng ~/.local/share/icons/hicolor/256x256/apps/
-  cp $iconsvg ~/.local/share/icons/hicolor/scalable/apps/
-#   cp $iconpng ~/.local/share/icons/
-#   cp $iconsvg ~/.local/share/icons/
-  exit
-fi
+mkdir -p $respath/share/applications/
+cp $desktop $respath/share/applications/
+mkdir -p $respath/share/icons/hicolor/scalable/apps/
+mkdir -p $respath/share/icons/hicolor/256x256/apps/
+cp $iconpng $respath/share/icons/hicolor/256x256/apps/
+cp $iconsvg $respath/share/icons/hicolor/scalable/apps/
