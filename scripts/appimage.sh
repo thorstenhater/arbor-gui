53fb54418e20813d46c5920ddc24123a483ed6ba
Brent Huisman <brent@huisman.pl>
2021-08-13 13:41:48 +0200

    lower version ubuntu

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -6,5 +6,4 @@ unset QT_PLUGIN_PATH
 unset LD_LIBRARY_PATH
 ./linuxdeployqt*.AppImage --appimage-extract-and-run ~/AppDir/share/applications/*.desktop -bundle-non-qt-libs -verbose=2
 ./linuxdeployqt*.AppImage --appimage-extract-and-run ~/AppDir/share/applications/*.desktop -appimage -verbose=2
-cp *.AppImage* /${GITHUB_WORKSPACE}/arbor-gui.AppImage
-ls -l /${GITHUB_WORKSPACE}/arbor-gui.AppImage
+cp arbor*.AppImage* /${GITHUB_WORKSPACE}/arbor-gui.AppImage
