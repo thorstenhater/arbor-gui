22a03954204f843395a29ca3357697d1d1fe62b5
Brent Huisman <brent@huisman.pl>
2021-08-17 15:40:21 +0200

    set march to x86-64-v2 without vectorization for max compat

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -3,13 +3,15 @@ if [ -z "$GITHUB_ACTIONS" ]; then CMAKECOMP=""; else CMAKECOMP="-DCMAKE_CXX_COMP
 bld_dir=$(mktemp -d)
 app_dir=$(mktemp -d)
 tmp_dir=$(mktemp -d)
-cmake -S . -B $bld_dir "$CMAKECOMP" --install-prefix $app_dir/usr -DARB_VECTORIZE=ON -DARB_ARCH=sandybridge
-cmake --build $bld_dir -j 4
-cmake --install $bld_dir
+cmake -S . -B $bld_dir "$CMAKECOMP" --install-prefix $app_dir/usr -DARB_ARCH=x86-64-v2
+# cmake --build $bld_dir -j 4
+# cmake --install $bld_dir
 cd "$tmp_dir"
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy-x86_64.AppImage
-# if [ -z "$GITHUB_ACTIONS" ]; then ; else
+# TODO: if versioning is solved, and we have a stable URL for releases, we might enable self-updating.
+# if [ ! -z "$GITHUB_ACTIONS" ]
+# then
 #     export ARCH=x86_64
 #     export UPDATE_INFORMATION="gh-releases-zsync|${GITHUB_REPOSITORY//\//|}|${VERSION:-"continuous"}|CPU-X-*$ARCH.AppImage.zsync"
 # fi
