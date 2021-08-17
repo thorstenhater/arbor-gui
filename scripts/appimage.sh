0029c52a84f1355779a6062c81ee7ac1aecb089a
Brent Huisman <brent@huisman.pl>
2021-08-17 15:52:07 +0200

    dont commit comments

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -4,8 +4,8 @@ bld_dir=$(mktemp -d)
 app_dir=$(mktemp -d)
 tmp_dir=$(mktemp -d)
 cmake -S . -B $bld_dir "$CMAKECOMP" --install-prefix $app_dir/usr -DARB_ARCH=x86-64-v2
-# cmake --build $bld_dir -j 4
-# cmake --install $bld_dir
+cmake --build $bld_dir -j 4
+cmake --install $bld_dir
 cd "$tmp_dir"
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy-x86_64.AppImage
