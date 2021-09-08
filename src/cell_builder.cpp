d03d223cad9097b529dc7f34e3442dd6fb2fb3f2
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2021-09-08 11:12:24 +0200

    Fix empty segments.

--- src/cell_builder.cpp
+++ src/cell_builder.cpp
@@ -45,7 +45,15 @@ void cell_builder::make_label_dict(std::vector<ls_def>& locsets, std::vector<rg_
 std::vector<arb::msegment> cell_builder::make_segments(const arb::region& region) {
   auto concrete = thingify(region, provider);
   auto result = pwlin.all_segments(concrete);
-  std::remove_if(result.begin(), result.end(), [](const auto& s) { return s.dist == s.prox; });
+  for (const auto& [id, p, d, t]: result) {
+    log_debug("Seg id={} prox=({}, {}, {}, {}) dist=({}, {}, {}, {}) tag={}",
+              id,
+              p.x, p.y, p.z, p.radius,
+              d.x, d.y, d.z, d.radius,
+              t);
+  }
+  std::erase_if(result,
+                [](const auto& s) {return distance(s.prox, s.dist) <= std::numeric_limits<double>::epsilon(); });
   return result;
 }
 
